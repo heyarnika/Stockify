620b6d4301ff471de01fb42efe77e09f0b607ad2
sanyuktaa15 <sugharde_b24@et.vjti.ac.in>
2026-01-26 00:56:19 +0530

    interconnection of internal pages (MUEHEHE)and remaking the navigation bar + the chatbot page with the chatbot section and without the graphs

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,25 @@ import Dashboard from './pages/Dashboard';
 import Predict from './pages/Predict';
 import Login from './pages/Login';          
 import CreateAccount from './pages/CreateAccount';
+import Advisor from './pages/Advisor'; // Ensure the filename is Advisor.jsx in your pages folder
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        {/* Public Routes */}
         <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/predict" element={<Predict />} />
-        
-        {/* FIX: Use curly braces for comments in JSX */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<CreateAccount />} />
+
+        {/* Private/Internal Routes */}
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/predict" element={<Predict />} />
+        <Route path="/advisor" element={<Advisor />} />
+        
+        {/* Note: If you ever want to add a "Page Not Found" 404, 
+          you would add a route with path="*" at the very bottom.
+        */}
       </Routes>
     </div>
   );
